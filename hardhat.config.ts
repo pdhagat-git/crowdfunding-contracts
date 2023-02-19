75ada5d80eb9959c5ff7de88948f9a7caec13dfb
Prabhansh Dhagat <prabhanshdhagat@gmail.com>
2023-02-20 00:43:13 +0530

    chore: update hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,22 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-etherscan";
+import "@nomiclabs/hardhat-ethers";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+	solidity: "0.8.17",
+	networks: {
+		goerli: {
+			url: process.env.RPC_URL,
+			accounts: [process.env.PRIVATE_KEY || ""],
+		},
+	},
+	etherscan: {
+		apiKey: process.env.ETHERSCAN_API_KEY,
+	},
 };
 
 export default config;
